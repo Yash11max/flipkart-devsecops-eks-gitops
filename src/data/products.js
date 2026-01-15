2eb8941f51185e000b96d51f8b0ca4ed54b3d204
Yash11max <shankhwalkary11@gmail.com>
2026-01-15 14:47:14 +0530

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -1,1 +1,26 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 15",
+    price: 79999,
+    image: "https://m.media-amazon.com/images/I/71d7rfSl0wL._SX679_.jpg",
+    category: "Mobiles",
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy S23",
+    price: 69999,
+    image: "https://m.media-amazon.com/images/I/61VfL-aiToL._SX679_.jpg",
+    category: "Mobiles",
+  },
+  {
+    id: 3,
+    name: "MacBook Air M2",
+    price: 114999,
+    image: "https://m.media-amazon.com/images/I/71f5Eu5lJSL._SX679_.jpg",
+    category: "Laptops",
+  },
+];
+
+export default products;
 
